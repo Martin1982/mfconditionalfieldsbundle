443a5fe9315f7cb2da1b41587d728c6f7d47b21d
Martin de Keijzer <martin@sportbitmanager.nl>
2024-07-18 13:42:40 +0200

    Add a row attribute based on the container config

--- src/Form/Extension/ConditionalFieldExtension.php
+++ src/Form/Extension/ConditionalFieldExtension.php
@@ -6,6 +6,7 @@ use Martin1982\MfConditionalFieldsBundle\Exception\ConditionalFieldException;
 use Martin1982\MfConditionalFieldsBundle\Rules\ConditionalRulesInterface;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,6 +21,16 @@ class ConditionalFieldExtension extends AbstractTypeExtension
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefined(['conditional_options']);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        if (!array_key_exists('conditional_options', $options)) {
+            return;
+        }
+
+        $this->buildRow($builder, $options);
+
     }
 
     public function buildView(FormView $view, FormInterface $form, array $options): void
@@ -50,8 +61,57 @@ class ConditionalFieldExtension extends AbstractTypeExtension
             throw new ConditionalFieldException('a valid `logic` value is required in conditional_options');
         }
 
-        if (!array_key_exists('rules', $conditionalOptions) || count($conditionalOptions['rules']) < 1) {
+        if (
+            !array_key_exists('rules', $conditionalOptions) ||
+            !is_array($conditionalOptions['rules']) ||
+            count($conditionalOptions['rules']) < 1
+        ) {
             throw new ConditionalFieldException('No rules defined');
         }
+
+        foreach ($conditionalOptions['rules'] as $rule) {
+            $this->validateRule($rule);
+        }
+    }
+
+    private function validateRule(array $rule): void
+    {
+        if (!array_key_exists('name', $rule)) {
+            throw new ConditionalFieldException('a valid `name` value is required in conditional_options');
+        }
+
+        if (!array_key_exists('operator', $rule) || !in_array($rule['operator'], [
+                ConditionalRulesInterface::OPERATOR_IS,
+                ConditionalRulesInterface::OPERATOR_IS_NOT,
+                ConditionalRulesInterface::OPERATOR_GREATER_THAN,
+                ConditionalRulesInterface::OPERATOR_LESS_THAN,
+                ConditionalRulesInterface::OPERATOR_CONTAINS,
+                ConditionalRulesInterface::OPERATOR_DOES_NOT_CONTAIN,
+                ConditionalRulesInterface::OPERATOR_BEGINS_WITH,
+                ConditionalRulesInterface::OPERATOR_DOES_NOT_BEGIN_WITH,
+                ConditionalRulesInterface::OPERATOR_ENDS_WITH,
+                ConditionalRulesInterface::OPERATOR_DOES_NOT_END_WITH,
+                ConditionalRulesInterface::OPERATOR_IS_EMPTY,
+                ConditionalRulesInterface::OPERATOR_IS_NOT_EMPTY,
+            ])) {
+            throw new ConditionalFieldException('a valid `operator` value is required in conditional_options');
+        }
+
+        if (!array_key_exists('value', $rule)) {
+            throw new ConditionalFieldException('a valid `value` value is required in conditional_options');
+        }
+    }
+
+    private function buildRow(FormBuilderInterface $builder, array &$typeOptions): void
+    {
+        if (!array_key_exists('container', $typeOptions['conditional_options'])) {
+            throw new ConditionalFieldException('container is required in conditional_options');
+        }
+
+        $typeOptions['row_attr'] = array_merge($typeOptions['row_attr'], [
+            'row_attr' => [
+                'class' => $typeOptions['conditional_options']['container'],
+            ],
+        ]);
     }
 }
