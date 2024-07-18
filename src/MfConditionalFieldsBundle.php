9cf41bf107b5652e073cd0f556bfd43d765735d2
Martin de Keijzer <martin@sportbitmanager.nl>
2024-07-18 11:52:23 +0200

    Load config in bundle

--- src/MfConditionalFieldsBundle.php
+++ src/MfConditionalFieldsBundle.php
@@ -2,17 +2,14 @@
 
 namespace Martin1982\MfConditionalFieldsBundle;
 
-use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class MfConditionalFieldsBundle extends AbstractBundle
 {
-    public function configure(DefinitionConfigurator $definition): void
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        $definition->rootNode()
-            ->children()
-                ->scalarNode('apcu_prefix')->defaultNull()->end()
-            ->end()
-        ;
+        $container->import('../config/services.php');
     }
 }
