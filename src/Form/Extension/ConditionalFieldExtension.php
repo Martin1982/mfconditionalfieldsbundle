aeec12ae0914d8141b2526bf0f482322f632d6c4
Martin de Keijzer <martin@sportbitmanager.nl>
2024-07-18 11:35:11 +0200

    Check conditional fields rules when applicable

--- src/Form/Extension/ConditionalFieldExtension.php
+++ src/Form/Extension/ConditionalFieldExtension.php
@@ -2,7 +2,56 @@
 
 namespace Martin1982\MfConditionalFieldsBundle\Form\Extension;
 
-class ConditionalFieldExtension
-{
+use Martin1982\MfConditionalFieldsBundle\Exception\ConditionalFieldException;
+use Martin1982\MfConditionalFieldsBundle\Rules\ConditionalRulesInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
+class ConditionalFieldExtension extends AbstractTypeExtension
+{
+    public static function getExtendedTypes(): iterable
+    {
+        return [AbstractType::class];
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefined(['conditional_options']);
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options): void
+    {
+        if (!array_key_exists('conditional_options', $options)) {
+            return;
+        }
+
+        $conditionalOptions = $options['conditional_options'];
+
+        if (!array_key_exists('container', $conditionalOptions)) {
+            throw new ConditionalFieldException('container is required in conditional_options');
+        }
+
+        if (!array_key_exists('action', $conditionalOptions) || !in_array($conditionalOptions['action'], [
+            ConditionalRulesInterface::ACTION_SHOW,
+            ConditionalRulesInterface::ACTION_HIDE,
+            ConditionalRulesInterface::ACTION_ENABLE,
+            ConditionalRulesInterface::ACTION_DISABLE,
+        ])) {
+            throw new ConditionalFieldException('a valid `action` value is required in conditional_options');
+        }
+
+        if (!array_key_exists('logic', $conditionalOptions) || !in_array($conditionalOptions['logic'], [
+            ConditionalRulesInterface::LOGIC_OR,
+            ConditionalRulesInterface::LOGIC_AND,
+        ])) {
+            throw new ConditionalFieldException('a valid `logic` value is required in conditional_options');
+        }
+
+        if (!array_key_exists('rules', $conditionalOptions) || count($conditionalOptions['rules']) < 1) {
+            throw new ConditionalFieldException('No rules defined');
+        }
+    }
 }
--- src/Rules/ConditionalRulesInterface.php
+++ src/Rules/ConditionalRulesInterface.php
@@ -4,5 +4,22 @@ namespace Martin1982\MfConditionalFieldsBundle\Rules;
 
 interface ConditionalRulesInterface
 {
-
+    public const ACTION_SHOW = 'show';
+    public const ACTION_HIDE = 'hide';
+    public const ACTION_ENABLE = 'enable';
+    public const ACTION_DISABLE = 'disable';
+    public const LOGIC_OR = 'or';
+    public const LOGIC_AND = 'and';
+    public const OPERATOR_IS = 'is';
+    public const OPERATOR_IS_NOT = 'isnot';
+    public const OPERATOR_GREATER_THAN = 'greaterthan';
+    public const OPERATOR_LESS_THAN = 'lessthan';
+    public const OPERATOR_CONTAINS = 'contains';
+    public const OPERATOR_DOES_NOT_CONTAIN = 'doesnotcontain';
+    public const OPERATOR_BEGINS_WITH = 'beginswith';
+    public const OPERATOR_DOES_NOT_BEGIN_WITH = 'doesnotbeginwith';
+    public const OPERATOR_ENDS_WITH = 'endswith';
+    public const OPERATOR_DOES_NOT_END_WITH = 'doesnotendwith';
+    public const OPERATOR_IS_EMPTY = 'isempty';
+    public const OPERATOR_IS_NOT_EMPTY = 'isnotempty';
 }
